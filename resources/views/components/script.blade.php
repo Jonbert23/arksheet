23d65bc7afbe37eb4ec377f59692c5c29a8b7cd3
Jonbert23 <jonbertandam@gmail.com>
2025-11-18 21:25:47 +0800

    fix: Replace local Iconify script with CDN version - Changed from local iconify-icon.min.js to CDN - Fixes missing icons in business sidebar - More reliable icon loading - Version 1.0.7 from code.iconify.design

--- resources/views/components/script.blade.php
+++ resources/views/components/script.blade.php
@@ -9,7 +9,7 @@
     <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
     <!-- Iconify Font js -->
-    <script src="{{ asset('assets/js/lib/iconify-icon.min.js') }}"></script>
+    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
     <!-- jQuery UI js -->
     <script src="{{ asset('assets/js/lib/jquery-ui.min.js') }}"></script>
     <!-- Vector Map js -->
